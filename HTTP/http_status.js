81555aee15cf5a8933e52c80e0b89ba1031db4b2
Yash <yash1402raj@gmail.com>
2024-10-19 21:20:03 +0530

    http_status

---
+++ HTTP/http_status.js
@@ -0,0 +1,14 @@
+/*
+    HTTP status code:- It indicate whether a specific HTTP request has been successfull completed:
+    These are group into five response:
+    1) Infromational Response(100-199)
+    2) Successfull Response(200-299)
+    3) Redirectional Response(300-399)
+    4) Client error response(400-499)
+    5) Server error response(500-599)
+
+
+    404- Not found
+    200- OK
+
+*/
