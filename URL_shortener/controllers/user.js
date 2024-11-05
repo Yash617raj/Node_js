b30d7cf8b838aed330b16551e93665b7c130a946
Yash <yash1402raj@gmail.com>
2024-11-05 22:08:47 +0530

    comments added

--- URL_shortener/controllers/user.js
+++ URL_shortener/controllers/user.js
@@ -1,29 +1,45 @@
-const User = require("../models/user")
-const {v4: uuidv4} = require("uuid");
-const {setUser} = require("../service/auth")
+const User = require("../models/user"); 
+const { v4: uuidv4 } = require("uuid"); 
+const { setUser } = require("../service/auth"); 
 
-async function handleSignUp(req,res){
-    const {name,email,password} = req.body
-    await User.create({
-        name,
-        email,
-        password,
-    })
-    return res.redirect("/");
+// Handles user signup process
+async function handleSignUp(req, res) {
+  const { name, email, password } = req.body; // Extracting name, email, and password from the request body
+
+  // Creating a new user in the database with the provided details
+  await User.create({
+    name,
+    email,
+    password,
+  });
+
+  // Redirecting the user to the home page after successful signup
+  return res.redirect("/");
 }
 
-async function handleLogin(req,res){
-    const {email,password} = req.body
-    const user = await User.findOne({email,password});
-    if(!user) {
-        res.render("login",{
-            error: "Invalid Username or Password"
-        })
-    }
-    const sessionId = uuidv4();
-    setUser(sessionId,user);
-    res.cookie("uid",sessionId);
-    return res.redirect("/");
+// Handles user login process
+async function handleLogin(req, res) {
+  const { email, password } = req.body; // Extracting email and password from the request body
+
+  // Searching for a user in the database with the provided email and password
+  const user = await User.findOne({ email, password });
+
+  // If user is not found, render the login page with an error message
+  if (!user) {
+    return res.render("login", {
+      error: "Invalid Username or Password",
+    });
+  }
+
+  // Generating a unique session ID and storing it in the session management system
+  const sessionId = uuidv4();
+  setUser(sessionId, user);
+
+  // Setting a cookie named "uid" with the session ID for identifying the user session
+  res.cookie("uid", sessionId);
+
+  // Redirecting the user to the home page after successful login
+  return res.redirect("/");
 }
 
 module.exports = {
