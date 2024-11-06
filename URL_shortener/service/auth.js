b24969232d8873dbd43f5e861109511aaacc0151
Yash <yash1402raj@gmail.com>
2024-11-06 22:03:10 +0530

    chota

--- URL_shortener/service/auth.js
+++ URL_shortener/service/auth.js
@@ -1,13 +1,13 @@
 // const sessionIdToUserMap = new Map(); // Map to store user sessions
 
-const jwt = require("jsonwebtoken");
-const secret = "yash$raj$123" 
+const jwt = require("jsonwebtoken"); 
+const secret = "yash$raj$123" // this is a secret that is available the with dev only so that nobody else could change the data without this
 
 // function setUser(id,user){
 //     sessionIdToUserMap.set(id,user);
 // }
 
-function setUser(user){
+function setUser(user){ // this generate a json web token
     return jwt.sign(
       {
         _id: user._id,
